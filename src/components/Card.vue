9e900f6bd6823b51842049c31454f3806f4463a9
Sammy Formighieri <sformighieri@uolinc.com>
2021-05-21 09:19:16 -0300

    commit inicial

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <h2>{{ subtitleCard }}</h2>
+        <p>{{ descriptionCard }}</p>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['dataCard'], 
+    data() {
+        return {
+            subtitleCard: this.dataCard.subtitleCard,
+            descriptionCard: this.dataCard.descriptionCard
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+      <template v-for="(card, i) in cards">
+          <Card :key="card.id" :dataCard="cards[i]"/>
+      </template>
+      <Footer />
+  </main>
+</template>
+
+<script>
+import Card from './Card.vue'
+import Footer from './Footer.vue'
+
+export default {
+    components: {
+        Card,
+        Footer
+    },
+    data() {
+        return {
+            cards: [
+                {
+                    id: 1,
+                    subtitleCard: 'Subtítulo 1',
+                    descriptionCard: 'la lalal alla alalla lalal'
+                },
+                {
+                    id: 2,
+                    subtitleCard: 'Subtítulo 2',
+                    descriptionCard: 'la lalal alla alalla lalal gdfhh fgds fdsff sfsdf sdfs'
+                },
+                {
+                    id: 3,
+                    subtitleCard: 'Subtítulo 3',
+                    descriptionCard: 'la lalal alla alalla lalal'
+                },
+                {
+                    id: 4,
+                    subtitleCard: 'Subtítulo 4',
+                    descriptionCard: 'la lalal alla alalla lalal gdfhh fgds fdsff sfsdf sdfs'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
